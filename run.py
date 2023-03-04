8dbd6176ff745bcad2df3da49f95e0fe9207081c
Mbrown <mbrown@corcentric.com>
2023-03-04 03:46:03 -0500

    added a master logger class with env based log locations

--- run.py
+++ run.py
@@ -1,77 +1,15 @@
-"""
-@author: Kuro
-"""
-import os
-
-from py_linq import Enumerable
-from settings import Config as config
+import uvicorn
+from settings import Config
 from app.endpoints.routes import add_routes
-from app import app
-import asyncio
-from uvicorn import Server, Config
-import logging
 
-logging.basicConfig(
-    filename=f"{os.getcwd()}/logs/runtime.log",
-    filemode="a",
-    level=logging.DEBUG,
-    format="%(asctime)s %(levelname)s %(name)s %(message)s",
-    datefmt="%Y-%m-%d %H:%M:%S",
-    force=True,
-)
-logger = logging.getLogger(__name__)
-logger.addHandler(logging.StreamHandler())
-logger.setLevel(logging.DEBUG)
-
-app = add_routes(app)
-
-
-class SocketServer(Server):
-    async def run(self, sockets=None):
-        self.config.setup_event_loop()
-        return await self.serve(sockets=sockets)
-
-
-async def run():
-    app_configs = [
-        {
-            "WebAPI": Config(
-                "app:app",
-                host=config.fastapi_host,
-                port=config.fastapi_port,
-                # ssl_keyfile="certs/local.key",
-                # ssl_certfile="certs/local.pem",
-                workers=config.workers,
-            )
-        },
-        {
-            "SocketIO": Config(
-                "app.rpc:app",
-                host=config.fastapi_host,
-                port=config.fastapi_port + 1,
-            )
-        },
-    ]
-
-    apps = [
-        SocketServer(config=Enumerable(_config.values()).first()).run()
-        for _config in app_configs
-    ]
-
-    return await asyncio.gather(*apps)
-
-
-if __name__ == "__main__":
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(run())
-
+app = add_routes()
 
 if __name__ == "__main__":
     uvicorn.run(
-        "app.rpc:app",
-        host=config.fastapi_host,
-        port=config.fastapi_port,
+        "app:app",
+        host=Config.fastapi_host,
+        port=Config.fastapi_port,
         # ssl_keyfile="certs/local.key",
         # ssl_certfile="certs/local.pem",
-        workers=config.workers,
+        workers=Config.workers,
     )
