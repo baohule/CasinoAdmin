377e603859df4ec8ed23a6359924e067c26d5a84
Mbrown <mbrown@corcentric.com>
2023-03-04 03:46:03 -0500

    added a master logger class with env based log locations

--- run.py
+++ run.py
@@ -1,13 +1,8 @@
 import uvicorn
 from settings import Config
 from app.endpoints.routes import add_routes
-from logging.config import dictConfig
-from fastapi.logger import logger
 
-dictConfig(Config.log_config)
 app = add_routes()
-
-logger.info(f"Starting application with {Config.workers} workers")
 
 if __name__ == "__main__":
     uvicorn.run(
