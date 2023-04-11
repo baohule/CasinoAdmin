892078d15eb5e5e4b9d03c361c285b8573d11da3
Michael Brown <kurosama112@gmail.com>
2023-04-11 15:33:25 -0400

    updated response schemas

--- app/api/agent/views.py
+++ app/api/agent/views.py
@@ -98,7 +98,6 @@ async def update_user(context: UpdateUser, request: Request):
     """
     data = context.dict(exclude_unset=True)
     updated_user = User.update_user(**data)
-    User.session.close()
     return (
         UpdateUserResponse(success=True, response=updated_user)
         if updated_user
@@ -111,14 +110,14 @@ async def remove_user(context: RemoveUser, request: Request) -> BaseResponse:
     """
     `Remove the user with the given id.`
 
-    :param context: RemoveUser - this is the context object that is passed to the function. It contains the id of the user to be removed
+    :param context: RemoveUser - this is the context object that is passed to the function.
+    It contains the id of the user to be removed
     :type context: RemoveUser
     :param request: Request - The request object that was sent to the server
     :type request: Request
     :return: The agent is being returned.
     """
     remove_agent = Agent.remove_agent(id=context.id)
-    Agent.session.close()
     return BaseResponse(success=True, response=remove_agent)
 
 
@@ -134,7 +133,6 @@ async def list_agents(context: GetAgentList, request: Request) -> PagedBaseRespo
     :return: A list of users.
     """
     paged_response = Agent.list_all_agents(context.params.page, context.params.size)
-    Agent.session.close()
     return PagedBaseResponse(success=True, response=paged_response)
 
 
@@ -143,14 +141,16 @@ async def get_agent(context: GetAgent, request: Request):
     """
     This function retrieves an agent object based on the provided ID and returns it as a response.
 
-    :param context: The context parameter is of type GetAgent, contains information about the request being made to retrieve an agent.
+    :param context: The context parameter is of type GetAgent, contains information about the
+    request being made to retrieve an agent.
     :type context: GetAgent
-    \   :param request: The `request` parameter is an instance of the `Request` class, which represents an HTTP request received by the server.
+    \   :param request: The `request` parameter is an instance of the `Request` class, which
+    represents an HTTP request received by the server.
 ]|) :type request: Request
-    :return: The function `get_agent` returns a `GetAgentResponse` object with a boolean `success` attribute set to `True` and an `Agent` object as the `response` attribute.
+    :return: The function `get_agent` returns a `GetAgentResponse` object with a boolean `success`
+    attribute set to `True` and an `Agent` object as the `response` attribute.
     """
     agent = Agent.read(id=context.id)
-    Agent.session.close()
     return GetAgentResponse(success=True, response=agent)
 
 
@@ -170,5 +170,4 @@ async def get_agent_users(
     agent_users = Agent.agent_users(
         context.context.filter.id, context.params.page, context.params.size
     )
-    Agent.session.close()
     return GetAgentUsersResponse(success=True, response=agent_users)
