86cb2c60f2a4a38c9a34b8b0e0e3147a83dce7f8
Michael Brown <kurosama112@gmail.com>
2023-04-16 12:43:57 -0400

    added password md5 hashing (insecure) and a rotation helper

---
+++ app/shared/helper/rotate_password_schemes.py
@@ -0,0 +1,34 @@
+from app.shared.auth.password_handler import get_password_hash, verify_password
+from app.api.user.models import User
+from app.api.agent.models import Agent
+from app.api.admin.models import Admin
+
+
+admins = Admin.read_all()
+users = User.read_all()
+agents = Agent.read_all()
+
+for admin in admins:
+    try:
+        admin.password = get_password_hash("123")
+        admin.session.commit()
+    except Exception as e:
+        print(e)
+        admin.session.rollback()
+
+for user in users:
+
+    try:
+        user.password = get_password_hash("123")
+        user.session.commit()
+    except Exception as e:
+        print(e)
+        user.session.rollback()
+
+for agent in agents:
+    try:
+        agent.password = get_password_hash("123")
+        agent.session.commit()
+    except Exception as e:
+        print(e)
+        agent.session.rollback()
