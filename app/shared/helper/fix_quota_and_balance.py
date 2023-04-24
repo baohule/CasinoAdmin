ebf3db34458e54f7a0a09c8dcf52ef1cf22b1bb3
Michael Brown <kurosama112@gmail.com>
2023-04-24 10:39:59 -0500

    fixed fk bug

--- app/shared/helper/fix_quota_and_balance.py
+++ app/shared/helper/fix_quota_and_balance.py
@@ -9,7 +9,7 @@ for agent in Agent.where():
     else:
         Quota.create(agentId=agent.id, balance=0)
     for user in agent.users:
-        if user.balance.amount:
+        if user.credit_account.balance:
             continue
         else:
             Balance.create(ownerId=user.id, balance=0)
