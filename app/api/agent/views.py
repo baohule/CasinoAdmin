c4ac23e75dc1801ce29a6ec3644e8de9d8c5502a
Michael Brown <kurosama112@gmail.com>
2023-04-24 20:50:35 -0500

    fixed agent list

--- app/api/agent/views.py
+++ app/api/agent/views.py
@@ -91,7 +91,7 @@ async def update_user(context: UpdateUser, request: Request):
     data = context.dict(exclude_unset=True)
     updated_user = User.update_user(**data)
     return (
-        UpdateUserResponse(success=True, response=context.dict(exclude_unset=True))
+        UpdateUserResponse(success=True, response=updated_user)
         if updated_user
         else BaseResponse(success=False, error="Object not updated")
     )
