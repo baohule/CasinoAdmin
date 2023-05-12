e6f7d9334586e7d4264073fa324d8577eb46747d
Michael Brown <kurosama112@gmail.com>
2023-05-12 12:02:39 -0400

    fixed the sockets to run on seperate ports

--- app/endpoints/urls.py
+++ app/endpoints/urls.py
@@ -5,3 +5,12 @@ class APIPrefix:
     """
 
     include = ["auth", "admin", "agent", "user", "credit", "game", "history"]
+
+
+class SocketPrefix:
+    """
+    Generic method to create routers in FastAPI so they are added to the swagger doc.
+    To use, add the route name as a string to the include list and your route will be built.
+    """
+
+    include = ["auth", "user", "credit", "game", "history", "notification"]
