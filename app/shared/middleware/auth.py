b03ff88e2c7c90175126208046fcce737ffd26eb
Michael Brown <kurosama112@gmail.com>
2023-05-19 23:51:46 -0400

    abstracted probability methods

--- app/shared/middleware/auth.py
+++ app/shared/middleware/auth.py
@@ -147,3 +147,4 @@ class JWTBearer(HTTPBearer, AuthenticationBackend):
         # logger.info(f"decoded_user: {decoded_user}")
         # logger.info(f"middleware_user_type: {middleware_user_type}")
         return AuthCredentials(["authenticated"]), middleware_user_type(decoded_user.id)
+
